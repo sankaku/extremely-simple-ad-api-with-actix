227be5403c80ef8819d4351c9d5192cb3939add4
sankaku <sankaku.git@gmail.com>
2021-03-21 07:26:00 +0900

    add logger

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,9 +1,10 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use bb8::Pool;
 use bb8_redis::{redis::AsyncCommands, RedisConnectionManager};
+use log::info;
 use redis::RedisError;
 
 // TODO: How to merge DeliveryResponse and CvResponse ?
@@ -52,6 +53,7 @@ async fn deliver(
     pool: web::Data<Pool<RedisConnectionManager>>,
 ) -> impl Responder {
     let num = params.num.unwrap_or(5);
+    info!("[deliver] num = {}", num);
 
     let ads = create_ads(num);
     record_delivered(&ads, &pool).await;
@@ -86,6 +88,8 @@ async fn cv(
     pool: web::Data<Pool<RedisConnectionManager>>,
 ) -> impl Responder {
     let id = params.id;
+    info!("[cv] id = {}", id);
+
     let res = CvResponse {
         success: true,
         errors: vec!["".to_string()],
@@ -111,10 +115,21 @@ async fn create_pool() -> Result<Pool<RedisConnectionManager>, RedisError> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
     let pool = create_pool().await.expect("Failed to create pool");
 
-    HttpServer::new(move || App::new().data(pool.clone()).service(deliver).service(cv))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(move || {
+        let logger = Logger::default();
+
+        App::new()
+            .data(pool.clone())
+            .wrap(logger)
+            .service(deliver)
+            .service(cv)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
