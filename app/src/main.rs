a5e4ae012341b1360cf01f5a96afcc24e5bdaffa
sankaku <sankaku.git@gmail.com>
2021-03-20 16:11:29 +0900

    impl cv (just echo posted id)

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,11 +2,29 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+// TODO: How to merge DeliveryResponse and CvResponse ?
 #[derive(Debug, Serialize, Deserialize)]
-struct ApiResponse {
+struct DeliveryResponse {
     success: bool,
     errors: Vec<String>,
+    message: DeliveryMessage,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CvResponse {
+    success: bool,
+    errors: Vec<String>,
+    message: CvMessage,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct DeliveryMessage {
     ads: Vec<Ad>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CvMessage {
+    id: Uuid,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -19,14 +37,30 @@ struct DeliveryParams {
     num: Option<u64>,
 }
 
+#[derive(Deserialize)]
+struct CvParams {
+    id: Uuid,
+}
+
 #[get("/deliver")]
 async fn deliver(params: web::Query<DeliveryParams>) -> impl Responder {
     let num = params.num.unwrap_or(5);
     let ads = create_ads(num);
-    let res = ApiResponse {
+    let res = DeliveryResponse {
         success: true,
         errors: vec!["".to_string()],
-        ads: ads,
+        message: DeliveryMessage { ads: ads },
+    };
+    HttpResponse::Ok().json(res)
+}
+
+#[post("cv")]
+async fn cv(params: web::Query<CvParams>) -> impl Responder {
+    let id = params.id;
+    let res = CvResponse {
+        success: true,
+        errors: vec!["".to_string()],
+        message: CvMessage { id: id },
     };
     HttpResponse::Ok().json(res)
 }
@@ -41,7 +75,7 @@ fn create_ads(num: u64) -> Vec<Ad> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(deliver))
+    HttpServer::new(|| App::new().service(deliver).service(cv))
         .bind("127.0.0.1:8080")?
         .run()
         .await
